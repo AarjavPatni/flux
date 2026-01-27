aeb9d0df7b826a327009fb94a5d2aa59ed83023e
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2026-01-26 19:37:57 -0500

    feat(streaming): add channel demo
    - Add src/streaming/channel_demo.rs implementing two demos: channel_demo and backpressure_demo
    - Demonstrates tokio::mpsc channel with capacity 20 and 3, producer/consumer pattern
    - Adds tests basic_channel_works and backpressure_works using #[tokio::test]

---
+++ src/streaming/channel_demo.rs
@@ -0,0 +1,66 @@
+// src/streaming/channel_demo.rs
+
+use std::time::Duration;
+
+use anyhow::Result;
+use tokio::{sync::mpsc, time::sleep};
+
+pub async fn channel_demo() -> Result<()> {
+    let (tx, mut rx) = mpsc::channel::<u64>(20);
+
+    let producer = tokio::spawn(async move {
+        for i in 1..=20 {
+            tx.send(i).await.unwrap();
+            println!("Producer: sending {}", i);
+        }
+    });
+
+    let consumer = tokio::spawn(async move {
+        while let Some(val) = rx.recv().await {
+            println!("Consumer: received {}", val);
+        }
+    });
+
+    producer.await?;
+    consumer.await?;
+
+    Ok(())
+}
+
+pub async fn backpressure_demo() -> Result<()> {
+    let (tx, mut rx) = mpsc::channel::<u64>(3);
+
+    let producer = tokio::spawn(async move {
+        for i in 1..=20 {
+            tx.send(i).await.unwrap();
+            println!("Producer: sending {}", i);
+        }
+    });
+
+    let consumer = tokio::spawn(async move {
+        while let Some(val) = rx.recv().await {
+            println!("Consumer: received {}", val);
+            sleep(Duration::from_millis(100)).await;
+        }
+    });
+
+    producer.await?;
+    consumer.await?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn basic_channel_works() {
+        channel_demo().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn backpressure_works() {
+        backpressure_demo().await.unwrap();
+    }
+}
